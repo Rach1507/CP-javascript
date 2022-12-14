88a12eea492572ae240a694a04f779d94c57e264
RachithaBR <59915937+Rach1507@users.noreply.github.com>
2022-12-14 17:13:41 +0530

    Update pivotIndex.js

--- pivotIndex.js
+++ pivotIndex.js
@@ -31,6 +31,21 @@ There is no index that satisfies the conditions in the problem statement.
  * @param {number[]} nums
  * @return {number}
  */
+
+var pivotIndex = function(nums) {
+
+    let sum = 0, leftsum = 0;
+    
+    for (let x of nums) sum += x;
+
+    for (let i = 0; i < nums.length; ++i) {
+        if (leftsum == sum - leftsum - nums[i]) return i;
+        leftsum += nums[i];
+    }
+    return -1;
+}
+
+
 var pivotIndex = function(nums) {
     let rightSum = sum(nums,0), leftSum = 0;
 
